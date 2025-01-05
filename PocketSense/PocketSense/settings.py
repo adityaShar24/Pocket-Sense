4ef73a12c48facd9264703eb0b226a58ed459e43
Aditya <adityarajansharma1234@gmail.com>
2025-01-05 15:22:24 +0530

    added : model Expense

--- PocketSense/PocketSense/settings.py
+++ PocketSense/PocketSense/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -152,6 +152,10 @@ LOGGING = {
 
 STATIC_URL = 'static/'
 
+DATA_DIR = BASE_DIR / 'data'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'data', 'receipts')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- PocketSense/expenses/models.py
+++ PocketSense/expenses/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 from django.conf import settings  
+from CoreAuth.models import (
+    Student,
+)
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)  # e.g., "Food", "Travel"
@@ -18,3 +21,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Expense(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    split_type = models.CharField(max_length=50)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateField()
+    receipt_image = models.ImageField(upload_to='receipts/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.amount} - {self.category} - {self.student} - {self.date}"
+
+    class Meta:
+        ordering = ['-date']
